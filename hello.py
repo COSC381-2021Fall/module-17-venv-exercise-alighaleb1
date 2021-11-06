cf09822858ed587c2060f2b9a7219ae28b3c5182
Ali Ghaleb <aghaleb1@emich.edu>
2021-11-06 02:07:47 +0000

    The first commit for Modul17 import and export exercise.

---
+++ hello.py
@@ -0,0 +1,13 @@
+import pyjokes
+import numpy
+import emoji
+import numpy as np
+
+print(emoji.emojize("python is :thumbs_up:"))
+print(emoji.emojize("Python is fun :red_heart:"))
+print(numpy.random.randint(5, size=(2, 4)))
+print(pyjokes.get_joke())
+
+a = np.array([1,2,3])
+print(a)
+
